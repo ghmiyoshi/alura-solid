64400b51edfb7154e9ef06d0915c329723199082
Gabriel Hideki Miyoshi <gabriel_hidekii@hotmail.com>
2022-12-17 16:32:06 -0300

    Extraindo validações (Polimorfismo)

---
+++ src/br/com/alura/rh/validacao/ValidacaoPercentualReajuste.java
@@ -0,0 +1,19 @@
+package br.com.alura.rh.validacao;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class ValidacaoPercentualReajuste implements ValidacaoReajuste {
+
+    @Override
+    public void validar(Funcionario funcionario, BigDecimal aumento) {
+        BigDecimal percentualReajuste = aumento.divide(funcionario.getSalario(), RoundingMode.HALF_UP);
+        if (percentualReajuste.compareTo(BigDecimal.valueOf(0.4)) > 0) {
+            throw new ValidacaoException("Reajuste não pode ser superior a 40% do salário");
+        }
+    }
+
+}
